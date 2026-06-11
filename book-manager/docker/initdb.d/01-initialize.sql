7a7b12415d9828841e908c807a5dba9fcfb7c190
itc-s25001 <s25001@std.it-college.ac.jp>
2026-06-11 17:00:20 +0900

    kotlin

--- book-manager/docker/initdb.d/01-initialize.sql
+++ book-manager/docker/initdb.d/01-initialize.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE book_manager;
 
-CREATE USER boot IDENTIFIED BY 'spring'
+CREATE USER boot IDENTIFIED BY 'spring';
 
 GRANT ALL PRIVILEGES ON book_manager.* TO boot;
---
+++ book-manager/src/main/kotlin/jp/ac/it_college/std/s25001/kotlin/book/manager/domain/type/RoleType.kt
@@ -0,0 +1,6 @@
+package jp.ac.it_college.std.s25001.kotlin.book.manager.domain.type
+
+enum class RoleType {
+    ADMIN,
+    USER
+}
